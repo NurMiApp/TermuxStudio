3abb77220c58e31a495301940d6d007781abd9ac
NurMi App <sasha.rostovskaya@bk.ru>
2024-08-13 10:02:25 +0300

    Update SH File

---
+++ package.sh
@@ -0,0 +1,13 @@
+# colors
+
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+reset='\033[0m'
+
+# Package
+printf "${red}[!] ${yellow}Do you type a PKG Bench?
+read choice
+if [ "${choice}" = "pkg benchmark test" ]
+
+fi
